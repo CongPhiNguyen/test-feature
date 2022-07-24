97e0f33bf3fdb7a577fd9bee4a4281b91b318f24
CongPhiNguyen <congphinguyen312@gmail.com>
2022-07-24 23:50:10 +0700

    test key press

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-let capturedInfo = document.querySelector(".captured-key");
-document.onkeypress = (e) => {
+let capturedInfo = document.querySelector('.captured-key');
+let keyType = document.querySelector('#key-type');
+
+const showKey = (e) => {
   let infoObject = {
-    "e.code": e.code,
-    "e.charCode": e.charCode,
-    "e.key": e.key,
-    "e.keyCode": e.keyCode,
+    'e.code': e.code,
+    'e.charCode': e.charCode,
+    'e.key': e.key,
+    'e.keyCode': e.keyCode,
   };
-  let infoString = "";
+  let infoString = '';
   Object.keys(infoObject).forEach((val) => {
-    infoString = infoString + `<br/>` + val + " : " + infoObject[val];
+    infoString = infoString + `<br/>` + val + ' : ' + infoObject[val];
   });
   capturedInfo.innerHTML = infoString;
   console.log(e);
+};
+
+document.addEventListener('keydown', showKey);
+
+keyType.onchange = () => {
+  console.log(keyType.value);
+  if (keyType.value === 'keypress') {
+    document.removeEventListener('keydown', showKey);
+    document.removeEventListener('keyup', showKey);
+    document.addEventListener('keypress', showKey);
+  } else if (keyType.value === 'keydown') {
+    document.removeEventListener('keypress', showKey);
+    document.removeEventListener('keyup', showKey);
+    document.addEventListener('keydown', showKey);
+  } else {
+    document.removeEventListener('keypress', showKey);
+    document.removeEventListener('keydown', showKey);
+    document.addEventListener('keyup', showKey);
+  }
 };
